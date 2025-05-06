666ff7f7937f8a7d20cd2b31ffa9b602c68e4aa7
paul <576330572@qq.com>
2025-05-06 16:13:36 +0800

    update tests

--- test/test.sh
+++ test/test.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
 ycc=../target/debug/ycc
-# set -e DO NOT add this 
+# set -e DO NOT add this
 
 try() {
-    # rm -rf tmp.s
-    
-    expected="$1"
-    input="$2"
-    # $ycc "$input" > tmp.s
-    $ycc "$input" > tmp.s
-    clang -o tmp tmp.s
-    ./tmp
-    actual="$?"
+	# rm -rf tmp.s
 
-    if [ "$actual" == "$expected" ]; then
-        echo "$input => $actual"
-    else
-        echo "$input expected, but got $actual"
-        exit 1
-    fi
+	expected="$1"
+	input="$2"
+	# $ycc "$input" > tmp.s
+	$ycc "$input" >tmp.s
+	clang -o tmp tmp.s
+	./tmp
+	actual="$?"
+
+	if [ "$actual" == "$expected" ]; then
+		echo "$input => $actual"
+	else
+		echo "$input expected, but got $actual"
+		exit 1
+	fi
 }
 
 echo "=================== build ==================== "
-cargo clean 
-cargo build 
+cargo clean
+cargo build
 
 echo "===================  test =================== "
 
 try 0 0
 try 42 42
 try 21 '5+20-4'
+try 36 '1+2+3+4+5+6+7+8'
 
 echo OK
+
--- test/tmp.s
+++ test/tmp.s
@@ -1,5 +1,5 @@
       .section __TEXT,__text
       .global _main
 _main:
-      mov w0, #42
+      mov w0, #1+2+3+4+5+6+7+8
       ret
