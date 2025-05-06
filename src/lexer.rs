9790faedcaf04fea026610681f2466f20f4c2284
paul <576330572@qq.com>
2025-05-06 15:47:47 +0800

    fix lexer and make test1 pass

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-use std::{f32::DIGITS, ffi::FromVecWithNulError, fs, string};
+pub type Index = usize;
 
 #[derive(Debug)]
 #[derive(Clone, PartialEq)]
@@ -9,33 +9,93 @@ pub enum BinaryOpKind {
     Div,
 }
 
+impl BinaryOpKind {
+
+    pub fn from_char(c: char) -> Self {
+        match c {
+            '+' => Self::Add,
+            '-' => Self::Minus,
+            '*' => Self::Multi,
+            '/' => Self::Div,
+            _ => panic!("Invalid binary operator: {}", c),
+        }
+    }
+
+}
+
 #[derive(Debug)]
 #[derive(Clone, PartialEq)]
 pub enum TokenKind {
     None, 
+    BinaryOp(BinaryOpKind),
+    Literal(LiteralKind),
+    Space,
+    EOF,
+}
+
+#[derive(Debug)]
+#[derive(Clone, PartialEq)]
+pub enum LiteralKind {
     IntNumber(i64),
     FloatNumber(f64),
-    BinaryOp(BinaryOpKind),
-    EOF,
+}
+
+#[derive(Debug)]
+#[derive(Clone, PartialEq)]
+pub struct Position {
+   pub(crate) line: Index,
+   pub(crate) column_start: Index,
+   pub(crate) column_end: Index,
+   pub(crate) is_eof: bool,
+}
+
+impl Position {
+
+    fn new(line: Index, column_start: Index, column_end: Index) -> Self {
+        Position {
+            line: line,
+            column_start: column_start,
+            column_end: column_end,
+            is_eof: false,
+        }
+    }
+
+    fn eof() -> Position {
+        Position {
+            line: 0,
+            column_start: 0,
+            column_end: 0,
+            is_eof: true,
+        }
+    }
 }
 
 #[derive(Debug)]
 #[derive(Clone, PartialEq)]
 pub struct Token {
     kind: TokenKind,
-    line: i64,
+    pos: Position,
     str: String, // TODO: wangya
 }
 
 impl Token  {
-    pub fn new(kind: TokenKind, line: i64, str: String ) -> Self {
-        Token { kind, line, str }
-    }   
+    pub fn new(kind: TokenKind, pos: Position, str: String ) -> Self {
+        Token { kind, pos, str }
+    }
+
+    pub fn eof() -> Token {
+        Token::new(TokenKind::EOF, Position::eof(), "".to_string())
+    }
 }
 
+// enum LexerError { // TODO: handle error
+//     UnexpectedCharacter{char: char},
+// }
+
 pub struct Lexer {
-    source_code: String, // TODO: wangya
-    
+    pre_index: Index,
+    current_index: Index,
+    source_code: String, 
 }
 
 impl Lexer {
@@ -51,23 +111,154 @@ impl Lexer {
 
     pub fn new(source_code: String) -> Self {
         Self {
-            source_code: source_code 
+            source_code: source_code,
+            pre_index: 0,
+            current_index: 0,
         }
     }
 
-    pub fn tokenize(self) -> Vec<Token> {
+    fn get_current(&self) -> Option<char> {
+        self.get_char(self.current_index)
+    }
+
+    fn get_char(&self, index: Index) -> Option<char> {
+        match index {
+            n if n < self.source_code.chars().count() => {
+                return self.source_code.chars().nth(index);
+            }
+            _ => {
+                return None;
+            }
+        }
+    }
+
+    // 查看下一个字符
+    fn peek(&self) -> Option<char> {
+        self.get_char(self.current_index + 1)
+    }
+
+    // 移动到下一个字符
+    fn next(&mut self) -> Option<char> {
+        self.current_index += 1;
+        self.get_current()
+    }
+
+    fn unread(&mut self) {
+        self.current_index -= 1;
+    }
+
+    fn generate_token(&mut self, kind: TokenKind) -> Token {
+        let range = self.pre_index..self.current_index+1;
+        let str = self.source_code.get(range).unwrap();
+        let pos = Position::new(1, self.pre_index, self.current_index);
+        let token = Token::new(kind, pos, str.to_string());
+        self.current_index += 1;
+        self.pre_index = self.current_index;
+        token
+    }
+
+    pub fn tokenize(mut self) -> Vec<Token> {
         let mut tks: Vec<Token> = Vec::new();
 
-        
-        for (i, c) in self.source_code.char_indices() {
-            if let Some(digit) = c.to_digit(10) {
-                let kind = TokenKind::IntNumber(digit as i64);
-                let token = Token::new(kind, 1, self.source_code[i..i+1].to_string());
-                tks.push(token);    
+        loop {
+            let token = self.scan().unwrap(); // TODO: wangya 
+            let is_eof = matches!(token.kind, TokenKind::EOF);
+            let is_space = matches!(token.kind, TokenKind::Space);
+            println!("token: {}", token.str);
+            if !is_space {
+                tks.push(token);
             }
-            println!("token {} :{}", i, c.to_string());
+            if is_eof {
+                break
+            }
         }
-
         tks
     }
+
+    fn get_current_str(&self) -> String {
+        self.get_str_from_range(self.pre_index, self.current_index)
+    }
+
+    fn get_str_from_range(&self, start: Index, end: Index) -> String {
+        self.source_code[start..end+1].to_string()
+    }
+
+    fn scan(&mut self) -> Option<Token> {
+        match self.get_current() {
+            Some(ch) => {
+                match ch {
+                    ' ' => {
+                        self.scan_space()
+                    },
+                    n if n == '+' || n == '-' || n == '*' || n == '/' => { // TODO: wangya
+                        self.scan_op()
+                    },
+                    n if n.is_digit(10) => {
+                        self.scan_number()
+                    },
+                    _ => {
+                        panic!("unexpected character: {}", ch);
+                    }
+                }
+           },
+           None => {
+                Some(Token::eof()) // TODO: is this right?
+           }
+        }
+    }
+    
+    fn scan_op(&mut self) -> Option<Token> {
+        self.scan_binary_op() // TODO: wangya ++ -- /= *=
+    }
+
+    fn scan_binary_op(&mut self) -> Option<Token> {
+        let ch = self.get_current().unwrap();
+        if ch == '+' || ch == '-' || ch == '*' || ch == '/' {
+            let kind = TokenKind::BinaryOp(BinaryOpKind::from_char(ch));
+            let token = self.generate_token(kind);
+            return Some(token);
+        }
+        panic!("unexpected character: {}", ch);
+    }
+
+    fn scan_space(&mut self) -> Option<Token> {
+        loop {
+            let ch = self.get_current().unwrap(); // TODO: wangya handle unwrap
+
+            if ch.is_whitespace() {
+                let next_ch = self.next().unwrap(); // TODO:
+
+                if !next_ch.is_whitespace() {
+                    self.unread();
+                    break;
+                }
+            } else {
+                break
+            }
+        }
+        Some(self.generate_token(TokenKind::Space))
+    }
+    
+    fn scan_number(&mut self) -> Option<Token> {
+        loop {
+            let next_ch = self.peek();
+            
+            if let Some(next_ch) = next_ch {
+                if next_ch.is_digit(10) {
+                    self.next();
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        // TODO: wangya 过于丑陋
+        let token_str = self.get_current_str();
+        let token_val = token_str.parse::<i64>().unwrap(); // TODO: wangya i64不准确
+        let kind = TokenKind::Literal(LiteralKind::IntNumber(token_val)); // TODO: wangya IntNumber不准确
+        let token = self.generate_token(kind);
+        Some(token)
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 // Reference: https://github.com/rui314/9cc
 use std::env;
 
-use lexer::{Lexer, Token};
+use lexer::{Lexer, Position, Token};
 mod lexer;
 
 fn main() {
@@ -23,15 +23,80 @@ fn main() {
     println!("      ret");
 }
 
-
-
 #[test]
 fn lexer_test_1() {
-   let lexer = Lexer::new("5 + 20 - 4".to_string());
-   let tokens = lexer.tokenize(); 
-   let token1 = Token::new(lexer::TokenKind::IntNumber(5), 1, "5".to_string());
-   let token1_vec = tokens.first().unwrap().clone();
-   assert_eq!(token1, token1_vec);
+    // 0123456789
+    // 5 + 20 - 4
+    let lexer = Lexer::new("5 + 20 -   4".to_string());
+    let tokens = lexer.tokenize();
+    let token1 = Token::new(
+        lexer::TokenKind::Literal(lexer::LiteralKind::IntNumber(5)),
+        Position {
+            line: 1,
+            column_start: 0,
+            column_end: 0,
+            is_eof: false,
+        },
+        "5".to_string(),
+    );
+    let token1_vec = tokens.first().unwrap().clone();
+    assert_eq!(token1, token1_vec);
+
+    let token2 = Token::new(
+        lexer::TokenKind::BinaryOp(lexer::BinaryOpKind::Add),
+        Position {
+            line: 1,
+            column_start: 2,
+            column_end: 2,
+            is_eof: false,
+        },
+        "+".to_string(),
+    );
+    let token2_vec = tokens.get(1).unwrap().clone();
+    assert_eq!(token2, token2_vec);
+
+    let token3 = Token::new(
+        lexer::TokenKind::Literal(lexer::LiteralKind::IntNumber(20)),
+        Position {
+            line: 1,
+            column_start: 4,
+            column_end: 5,
+            is_eof: false,
+        },
+        "20".to_string(),
+    );
+    let token3_vec = tokens.get(2).unwrap().clone();
+    assert_eq!(token3, token3_vec);
+
+    let token4 = Token::new(
+        lexer::TokenKind::BinaryOp(lexer::BinaryOpKind::Minus),
+        Position {
+            line: 1,
+            column_start: 7,
+            column_end: 7,
+            is_eof: false,
+        },
+        "-".to_string(),
+    );
+    let token4_vec = tokens.get(3).unwrap().clone();
+    assert_eq!(token4, token4_vec);
+
+    let token5 = Token::new(
+        lexer::TokenKind::Literal(lexer::LiteralKind::IntNumber(4)),
+        Position {
+            line: 1,
+            column_start: 11,
+            column_end: 11,
+            is_eof: false,
+        },
+        "4".to_string(),
+    );
+    let token5_vec = tokens.get(4).unwrap().clone();
+    assert_eq!(token5, token5_vec);
+
+    let token6 = Token::eof();
+    let token6_vec = tokens.get(5).unwrap().clone();
+    assert_eq!(token6, token6_vec);
 }
 
 // #[test]
@@ -49,7 +114,6 @@ fn lexer_test_1() {
 //     for path in example_paths {
 //         let name = path.unwrap().path().to_str().unwrap().to_string();
 
-//         writeln!(&mut stderr(), "comparing {} ...", name).unwrap();   
+//         writeln!(&mut stderr(), "comparing {} ...", name).unwrap();
 //     }
 // }
-
